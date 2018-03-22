40069f07bb24e02c0d55f389771199a8e62a2a16
David González <nodearth@gmail.com>
2018-03-22 13:12:07 +0000

    exposing dirname for vulns on core and npm

---
+++ index.js
@@ -0,0 +1,5 @@
+const path = require('path')
+module.exports.npmPath = path.join(__dirname, 'security-wg-master', 'vuln', 'npm')
+module.exports.corePath = path.join(__dirname, 'security-wg-master', 'vuln', 'core')
+
+
